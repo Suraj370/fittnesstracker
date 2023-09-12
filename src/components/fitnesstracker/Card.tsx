ab15c097f0a26a26fda2d76aadb842467151563e
Suraj370 <surajpanda2077@gmail.com>
2023-09-12 15:51:36 +0530

    Modified Card to display if exercise completed or not

--- src/components/fitnesstracker/Card.tsx
+++ src/components/fitnesstracker/Card.tsx
@@ -1,37 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 import { useTodos } from "@/store/exercises";
-import { FitnessData } from './FitnessData';
+import { FitnessData } from "./FitnessData";
 import { EraserIcon } from "@radix-ui/react-icons";
 
-const Card = ({exercise}: {exercise: FitnessData}) => {
-    const { handleDeleteTodo, toggleTodoAsCompleted} = useTodos();
-
+const Card = ({ exercise }: { exercise: FitnessData }) => {
+  const { handleDeleteTodo, toggleTodoAsCompleted } = useTodos();
+  const [isCompleted, setIsCompleted] = useState(exercise.completed);
   return (
     <div
-    className="flex justify-between rounded-lg bg-white mb-2 px-4 py-4 text-gray-900 shadow"
-    key={exercise.id}
-  >
-    <div>
-      <div className='flex items-center gap-2'>
-      <input type="checkbox" id={`todo-${exercise.id}`} checked={exercise.completed} onChange={() => {
-                            toggleTodoAsCompleted(exercise.id)}
-                        } />
-
-      <p>{exercise.exercise}</p>
-      
+      className="flex justify-between rounded-lg bg-white mb-2 px-4 py-4 text-gray-900 shadow"
+      key={exercise.id}
+    >
+      <div>
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            id={`todo-${exercise.id}`}
+            checked={exercise.completed}
+            onChange={() => {
+              setIsCompleted(!isCompleted);
+              toggleTodoAsCompleted(exercise.id);
+            }}
+          />
+          <div>
+            <p>{exercise.exercise}</p>
+            <p className="text-sm text-gray-500">Reps: {exercise.reps}</p>
+            <p className="text-sm text-gray-500">
+              {isCompleted ? "Completed" : "Not Completed"}
+            </p>
+          </div>
+        </div>
       </div>
-      <p className="text-sm text-gray-500">Reps: {exercise.reps}</p>
-   
-    </div>
 
-    <div>
+      <div>
         <button onClick={() => handleDeleteTodo(exercise.id)}>
-        <EraserIcon/>
+          <EraserIcon />
         </button>
-        
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
